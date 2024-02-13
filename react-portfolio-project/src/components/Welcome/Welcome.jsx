e05b8340c38f40febab38145e4657ce83559d5d7
Likhaya <99352117+likeLikhaya@users.noreply.github.com>
2024-02-13 15:14:49 +0200

    adding font to home page

--- react-portfolio-project/src/components/Welcome/Welcome.jsx
+++ react-portfolio-project/src/components/Welcome/Welcome.jsx
@@ -5,40 +5,36 @@ const Welcome = () => {
   return (
     <>
       <header>
-        <div class="navbar">
-          <a href="#" class="logo">
+        <div className="navbar">
+          <a href="#" className="logo">
             KALIMASHE
           </a>
-          <div class="navigation">
-            <div class="navitems">
-              <div class="navclosebtn"></div>
+          <div className="navigation">
+            <div className="navitems">
+              <div className="navclosebtn"></div>
               <a href="#About">Meet Likhaya</a>
               <a href="#Skills">Talent Bank</a>
               <a href="#Carousel">Creations</a>
               <a href="#Contact">Let's Connect</a>
             </div>
           </div>
-          <div class="navmenubtn"></div>
+          <div className="navmenubtn"></div>
         </div>
       </header>
 
       <section>
-        <div>
-          <span className="hello-there-text">
-            <h2 className="welcome">Hello There !</h2>
-          </span>
-          <div className="welcomeIntro">
-            <p>Welcome to my Digital Wonderland! 🚀</p>
-            <p>
-              This is where pixels come alive, code speaks, and innovation
-              reigns supreme! Get ready to embark on an adventure where every
-              project is a story waiting to be told - a tale of precision,
-              problem-solving skills, and creativity. Here, The Office isn't
-              just a show; it's a wellspring of inspiration. Are you ready to
-              explore the extraordinary? Let the digital adventure begin!
-            </p>
-          </div>
-        </div>
+        <span className="hello-there-text">Hello There !</span>
+
+        <p className="welcomeIntro">
+          Welcome to my Digital Wonderland! 🚀
+          <br />
+          This is where pixels come alive, code speaks, and innovation reigns
+          supreme! Get ready to embark on an adventure where every project is a
+          story waiting to be told - a tale of precision, problem-solving
+          skills, and creativity. Here, The Office isn't just a show; it's a
+          wellspring of inspiration. Are you ready to explore the extraordinary?
+          Let the digital adventure begin!
+        </p>
       </section>
     </>
   );
