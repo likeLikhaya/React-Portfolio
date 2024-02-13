7a8691c80cf4701af26c4769a61e78f38ba1aaab
Likhaya <99352117+likeLikhaya@users.noreply.github.com>
2024-02-13 09:56:14 +0200

    creating the footer

---
+++ react-portfolio-project/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <ul>
+          <li>
+            <a href="#Welcome">Welcome</a>
+          </li>
+          <li>
+            <a href="#About">Meet Likhaya </a>
+          </li>
+          <li>
+            <a href="#Skills">Talent Bank</a>
+          </li>
+          <li>
+            <a href="#Carousel">Creations</a>
+          </li>
+          <li>
+            <a href="#Contact">Let's get in touch</a>
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
