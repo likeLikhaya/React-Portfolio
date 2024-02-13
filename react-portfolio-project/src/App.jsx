ff41a4822061e33ac50af9a09cfd6ad5fe53b5a5
Likhaya <99352117+likeLikhaya@users.noreply.github.com>
2024-02-13 13:49:07 +0200

    editing the welcome page css

--- react-portfolio-project/src/App.jsx
+++ react-portfolio-project/src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
 // import Skills from "./components/Skills";
 // import About from "./components/About";
-//import Welcome from "./components/Welcome";
-import Carousel from "./components/Carousel";
-import CarouselItem from "./components/CarouselItem";
+import Welcome from "./components/Welcome/Welcome";
+//import Carousel from "./components/Carousel";
+//import CarouselItem from "./components/CarouselItem";
 import "./index.css";
 
 function App() {
   return (
     <>
-      {/* <Welcome />
-       <About />
-      <Skills />  */}
-      <CarouselItem />
+      <Welcome />
+      {/* <About />
+      <Skills />  
+      <CarouselItem /> */}
     </>
   );
 }
--- react-portfolio-project/src/components/Welcome/Welcome.jsx
+++ react-portfolio-project/src/components/Welcome/Welcome.jsx
@@ -1,62 +1,46 @@
 import React from "react";
+import "./Welcome.css";
 
 const Welcome = () => {
   return (
-    <section>
-      <header className="nav-bar">
-        <a href="/" className="logo">
-          Kalimashe
-        </a>
-        <div className="navigation">
-          <div className="nav-items">
-            <div className="nav-close-btn"></div>
-            <a href="#About" className="active">
-              Meet Likhaya
-            </a>
-            <nav className="navbar">
-              <ul className="nav-menu">
-                <li className="nav-item">
-                  <a href="#home" className="nav-link">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="#about" className="nav-link">
-                    About
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="#services" className="nav-link">
-                    Services
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="#contact" className="nav-link">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </nav>
+    <>
+      <header>
+        <div class="navbar">
+          <a href="#" class="logo">
+            KALIMASHE
+          </a>
+          <div class="navigation">
+            <div class="navitems">
+              <div class="navclosebtn"></div>
+              <a href="#About">Meet Likhaya</a>
+              <a href="#Skills">Talent Bank</a>
+              <a href="#Carousel">Creations</a>
+              <a href="#Contact">Let's Connect</a>
+            </div>
           </div>
+          <div class="navmenubtn"></div>
         </div>
       </header>
-      <div>
-        <span className="hello-there-text">
-          <p>Hello There !</p>
-        </span>
-        <div className="welcomeIntro">
-          <p>Welcome to my Digital Wonderland! 🚀</p>
-          <p>
-            This is where pixels come alive, code speaks, and innovation reigns
-            supreme! Get ready to embark on an adventure where every project is
-            a story waiting to be told - a tale of precision, problem-solving
-            skills, and creativity. Here, The Office isn't just a show; it's a
-            wellspring of inspiration. Are you ready to explore the
-            extraordinary? Let the digital adventure begin!
-          </p>
+
+      <section>
+        <div>
+          <span className="hello-there-text">
+            <h2 className="welcome">Hello There !</h2>
+          </span>
+          <div className="welcomeIntro">
+            <p>Welcome to my Digital Wonderland! 🚀</p>
+            <p>
+              This is where pixels come alive, code speaks, and innovation
+              reigns supreme! Get ready to embark on an adventure where every
+              project is a story waiting to be told - a tale of precision,
+              problem-solving skills, and creativity. Here, The Office isn't
+              just a show; it's a wellspring of inspiration. Are you ready to
+              explore the extraordinary? Let the digital adventure begin!
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
