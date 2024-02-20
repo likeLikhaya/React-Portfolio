aa379f767d6fd739eca23595e40c7d64e47b725d
Likhaya <kalimashelikhaya3@gmail.com>
2024-02-20 21:18:31 +0200

    added about image and download cv button

--- react-portfolio-project/src/components/About/About.jsx
+++ react-portfolio-project/src/components/About/About.jsx
@@ -1,34 +1,40 @@
 import React from "react";
+import "./About.css"
 
 const About = () => {
   return (
-    <section>
-      <div className="content-section" style={{ maxWidth: "600px", margin: "0 auto" }}>
-        <div className="about-img">
-          <img src=""/>
-        </div>
-        <p>
+    
+    <><div className="heading">
+      <h1>Meet Likhaya</h1>
+     
+    </div><div className="container">
+      <section className="about">
+        <div className="about-image">
+          <img src="src/images/profile-pic.png" alt="" />
+          </div>
+          <div className="about-content">
+          <p>
           I am a skilled coder who creates innovative products using
           caffeine-powered code spells. During the day, I work in the tech
           industry, transforming computer code into beautiful, functional
           systems. When I'm not working, I love to watch K-dramas and The Office
           for a good laugh.
-        </p>
-        <br />
-        <p>
+        
           But sometimes I need a break from the digital world, and that's when I
           head to the beach. The soothing sound of the waves and the fresh sea
           air helps me de-stress and recharge. It's a peaceful escape from the
           complexity of coding.
-        </p>
-        <br />
-        <p>
+        
           Throughout all of this, my friends and family keep me grounded. They
           support me and keep me sane in the ever-changing world of tech.
         </p>
-        <a href="cv/CV of Likhaya Kalimashe.pdf" download="Likhaya Kalimashe's CV.pdf"class="btn">Download CV <i class="fas fa-download"></i></a>
-      </div>
-    </section>
+        <a href="" className="download-cv"> Download CV</a>
+          </div>
+          </section>
+          </div>
+          </>
+     
+   
   );
 };
 
