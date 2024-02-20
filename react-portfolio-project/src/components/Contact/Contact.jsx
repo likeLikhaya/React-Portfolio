ca58cc84c1ccaf8249b2a090ed57b69dc763b55b
Likhaya <99352117+likeLikhaya@users.noreply.github.com>
2024-02-20 10:27:48 +0200

    finished with contact form

--- react-portfolio-project/src/components/Contact/Contact.jsx
+++ react-portfolio-project/src/components/Contact/Contact.jsx
@@ -1,25 +1,49 @@
 import React from "react";
-import "./Contact.css"
+import "./Contact.css";
 
 const Contact = () => {
-  return <div className="contact-container">
-    <form action="" className="contact-left">
-      <div className="contact-left-title">
-        <h2>Have a project idea?</h2>
-        <h2>Let's get in touch.</h2>
-        <hr/>
-      </div>
-      <input type="text" name="name" placeholder="Name" className="contact-inputs" required/>
-      <input type="text" name="surname" placeholder="Surname" className="contact-inputs" required/>
-      <input type="email" name="email" placeholder="Email" className="contact-inputs" required/>
-      <textarea name="message" placeholder="Subject" className="contact-inputs" required></textarea>
-      <button type="submit"> Send! </button>
-    </form>
-    <div className="contact-right">
-
-
+  return (
+    <div className="contact-container">
+      <form action="" className="contact-left">
+        <div className="contact-left-title">
+          <h2>Have a project idea?</h2>
+          <h2>Let's get in touch.</h2>
+          <hr />
+        </div>
+        <div className="input-group">
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="contact-inputs"
+            required
+          />
+          <input
+            type="text"
+            name="surname"
+            placeholder="Surname"
+            className="contact-inputs"
+            required
+          />
+        </div>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="contact-inputs"
+          required
+        />
+        <textarea
+          name="message"
+          placeholder="Subject"
+          className="contact-inputs"
+          required
+        ></textarea>
+        <button type="submit"> Send! </button>
+      </form>
+      <div className="contact-right"></div>
     </div>
-  </div>;
+  );
 };
 
 export default Contact;
