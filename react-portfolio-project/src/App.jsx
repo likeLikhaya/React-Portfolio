e67ff32e5853a32a724d016d650f2b9a50ca707f
Likhaya <99352117+likeLikhaya@users.noreply.github.com>
2024-02-20 13:55:50 +0200

    trying to add icons to skills page

--- react-portfolio-project/src/App.jsx
+++ react-portfolio-project/src/App.jsx
@@ -1,23 +1,24 @@
 import "./App.css";
-// import Skills from "./components/Skills";
-// import About from "./components/About";
+import Skills from "./components/Skills/Skills";
+// import About from "./components/About/About";
 //import Welcome from "./components/Welcome/Welcome";
 //import Slider from "./components/Slider/Slider";
 //import Footer from "./components/Footer/Footer";
-import Contact from "./components/Contact/Contact";
+//import Contact from "./components/Contact/Contact";
 
 import "./index.css";
 
 function App() {
   return (
     <>
+      <Skills />
       {/* <Welcome />
       <About />
-      <Skills />  
+       
       <Slider />
+      <Contact />
       <Footer />
         */}
-      <Contact />
     </>
   );
 }
--- react-portfolio-project/src/components/Skills/Skills.jsx
+++ react-portfolio-project/src/components/Skills/Skills.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "./Skills.css";
 
 const Skills = () => {
-  return <div>Talent Bank</div>;
+  return (
+    <div className="page-container">
+      <div className="skills-page">
+        <div className="frontend-skills">
+          <h2>Frontend Skills</h2>
+          <ul>
+            <li>
+              <i className="fab fa-html5"></i> HTML5
+            </li>
+            <li>
+              <i className="fab fa-css3-alt"></i> CSS3
+            </li>
+            <li>
+              <i className="fab fa-js"></i> JavaScript
+            </li>
+            {/* Add more frontend skills as needed */}
+          </ul>
+        </div>
+        <div className="backend-skills">
+          <h2>Backend Skills</h2>
+          <ul>
+            <li>
+              <i className="fas fa-database"></i> Database Management
+            </li>
+            <li>
+              <i className="fab fa-node-js"></i> Node.js
+            </li>
+            {/* Add more backend skills as needed */}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
-
 export default Skills;
