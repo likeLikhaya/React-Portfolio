44c4641198a5d22a7d4db7b32072d4a4f2299726
Likhaya <99352117+likeLikhaya@users.noreply.github.com>
2024-02-21 14:46:04 +0200

    changing the slider style

--- react-portfolio-project/src/components/Slider/Slider.jsx
+++ react-portfolio-project/src/components/Slider/Slider.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Slider from "react-slick";
+import SliderComponent from "react-slick"; // Rename the imported Slider component
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-//import "./Slider.css";
+import "./Slider.css"; // Make sure to adjust the path to your CSS file
 
-const Carousel = () => {
+const Slider = () => {
+  // Rename the local variable to avoid conflict
   const settings = {
     dots: true,
     infinite: true,
@@ -16,23 +17,89 @@ const Carousel = () => {
   return (
     <div className="wrapper">
       <Slider {...settings}>
-        <div>
-          <div className="carousel-item">
-            <img src="src/images/kuyasa.png" alt="Kuyasa Health Center" />
-            <div className="text-box">
-              <p>
-                Kuyasa Health Center is a user-friendly website built with HTML,
-                CSS, and JavaScript, offering a seamless experience for
-                scheduling appointments with healthcare professionals and
-                accessing mental health services.
-              </p>
+        <div className="new-feature-slider">
+          <div className="feature-slide">
+            <div className="new-feature-image-wrap">
+              <div className="feature-slide-image">
+                <img
+                  src="http://www.williamsjettenders.com/wp-content/uploads/2015/10/features41.jpg"
+                  alt="Feature 1"
+                />
+              </div>
+            </div>
+            <div className="center-content">
+              <div className="overview-text">
+                <h2 className="fakeh1">Cleaner, more powerful engine</h2>
+                <span className="divider-wave blue-wave"></span>
+                <p>
+                  Get improved power, torque and fuel efficiency with the
+                  next-generation Textron 850MPE engine. It performs better and
+                  is also cleaner, thanks to a catalytic converter, making it
+                  one of the cleanest marine engines in the world. Available in
+                  a choice of power outputs, there’s also a power reduction
+                  switch for jet tender newcomers who want to take it easy at
+                  first.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Second slide */}
+          <div className="feature-slide">
+            <div className="new-feature-image-wrap">
+              <div className="feature-slide-image">
+                <img
+                  src="http://www.williamsjettenders.com/wp-content/uploads/2015/08/325-image3.jpg"
+                  alt="Feature 2"
+                />
+              </div>
+            </div>
+            <div className="center-content">
+              <div className="overview-text">
+                <h2 className="fakeh1">High Performance</h2>
+                <span className="divider-wave blue-wave"></span>
+                <p>
+                  Turbojets are known for outperforming almost all comparable
+                  craft on the water, whether it is pulling a waterskier,
+                  cruising along the coast or whipping in and out of tight
+                  corners. With top speeds of up to 53 mph/85 kph, jumping
+                  aboard a Turbojet and firing up the engine is a thrill you
+                  will never tire of.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Third slide */}
+          <div className="feature-slide">
+            <div className="new-feature-image-wrap">
+              <div className="feature-slide-image">
+                <img
+                  src="http://www.williamsjettenders.com/wp-content/uploads/2015/08/445-image2.jpg"
+                  alt="Feature 3"
+                />
+              </div>
+            </div>
+            <div className="center-content">
+              <div className="overview-text">
+                <h2 className="fakeh1">Supreme Stability &amp; Buoyancy</h2>
+                <span className="divider-wave blue-wave"></span>
+                <p>
+                  The Turbojet design places the helmsman at the stern of the
+                  craft in the most secure position for maximum control. By
+                  utilising a centrally mounted engine and an inflatable tube,
+                  which is positioned close to the water, the Turbojet is
+                  supremely stable whether static or running at speed. This
+                  makes for easy and comfortable passenger boarding and a safe,
+                  enjoyable ride at high speed. What’s more, due to the inherent
+                  hull buoyancy, the tender will float even without the
+                  inflatable tube.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        {/* Add more slides as needed */}
       </Slider>
     </div>
   );
 };
 
-export default Carousel;
+export default SliderComponent;
