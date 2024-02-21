dd8031d5ac9b3f36c7074acdfaf59121f24c6a7e
Likhaya <99352117+likeLikhaya@users.noreply.github.com>
2024-02-21 10:21:21 +0200

    adjusting the slider

--- react-portfolio-project/src/components/Skills/Skills.jsx
+++ react-portfolio-project/src/components/Skills/Skills.jsx
@@ -3,51 +3,76 @@ import "./Skills.css";
 
 const Skills = () => {
   return (
-    <div className="container">
-      <div className="skill-box">
-        <div className="skills-title">
-          <div className="img">
-            <img src="src/images/html-icon.svg" alt="HTML Icon" className="skill-icon" />
+    <div>
+      <h1> Talent Bank</h1>
+
+      <div className="container">
+        <div className="skill-box">
+          <div className="skills-title">
+            <div className="img frontend">
+              <img
+                src="src/images/html-icon.svg"
+                alt="HTML Icon"
+                className="skill-icon"
+              />
+            </div>
+
+            <h3>HTML 5</h3>
           </div>
-          <h3>HTML 5</h3>
+          <p>add some text here maybe</p>
         </div>
-        <p>add some text here maybe</p>
-      </div>
-      <div className="skill-box">
-        <div className="skills-title">
-          <div className="img">
-            <img src="src/images/css-icon.svg" alt="CSS Icon" className="skill-icon" />
+        <div className="skill-box">
+          <div className="skills-title">
+            <div className="img frontend">
+              <img
+                src="src/images/css-icon.svg"
+                alt="CSS Icon"
+                className="skill-icon"
+              />
+            </div>
+            <h3>CSS</h3>
           </div>
-          <h3>CSS</h3>
+          <p>add some text here maybe</p>
         </div>
-        <p>add some text here maybe</p>
-      </div>
-      <div className="skill-box">
-        <div className="skills-title">
-          <div className="img">
-            <img src="src/images/js-icon.svg" alt="JavaScript Icon" className="skill-icon" />
+        <div className="skill-box">
+          <div className="skills-title">
+            <div className="img frontend">
+              <img
+                src="src/images/js-icon.svg"
+                alt="JavaScript Icon"
+                className="skill-icon"
+              />
+            </div>
+            <h3>JavaScript</h3>
           </div>
-          <h3>JavaScript</h3>
+          <p>add some text here maybe</p>
         </div>
-        <p>add some text here maybe</p>
-      </div>
-      <div className="skill-box">
-        <div className="skills-title">
-          <div className="img">
-            <img src="src/images/icons8-react.svg" alt="React Icon" className="skill-icon" />
+        <div className="skill-box">
+          <div className="skills-title">
+            <div className="img frontend">
+              <img
+                src="src/images/icons8-react.svg"
+                alt="React Icon"
+                className="skill-icon"
+              />
+            </div>
+            <h3>React</h3>
           </div>
-          <h3>React</h3>
+          <p>add some text here maybe</p>
         </div>
-        <p>add some text here maybe</p>
-      </div>
-      <div className="skill-box">
-        <div className="skills-title">
-          <div className="img">
-            <img src="src/images/icons8-nodejs-64.svg" alt="Node.js Icon" className="skill-icon" />
+        <div className="skill-box">
+          <div className="skills-title">
+            <div className="img backend">
+              <img
+                src="src/images/icons8-nodejs-64.svg"
+                alt="Node.js Icon"
+                className="skill-icon"
+              />
+            </div>
+            <h3>Node.js</h3>
           </div>
-          <h3>Node.js</h3>
+          <p>add some text here maybe</p>
         </div>
-        <p>add some text here maybe</p>
       </div>
     </div>
   );
--- react-portfolio-project/src/components/Slider/Slider.jsx
+++ react-portfolio-project/src/components/Slider/Slider.jsx
@@ -4,28 +4,63 @@ import "./Slider.css";
 const Slider = () => {
   return (
     <div id="slider">
-      <input type="radio" name="slider" id="slide1" defaultChecked />
+      <input type="radio" name="slider" id="slide1" checked />
       <input type="radio" name="slider" id="slide2" />
       <input type="radio" name="slider" id="slide3" />
+      <input type="radio" name="slider" id="slide4" />
       <div id="slides">
         <div id="overflow">
           <div className="inner">
             <div className="slide slide_1">
               <div className="slide-content">
-                <h2>Slide 1</h2>
-                <p>Content for slide 1</p>
+                <div className="content-wrapper">
+                  <div className="text-content">
+                    <h2>Slide 1</h2>
+                    <p>Content for Slide 1</p>
+                  </div>
+                  <div className="image-content">
+                    <img src="src/images/bug-bank.jpg" alt="Slide 1 Image" />
+                  </div>
+                </div>
               </div>
             </div>
             <div className="slide slide_2">
               <div className="slide-content">
-                <h2>Slide 2</h2>
-                <p>Content for slide 2</p>
+                <div className="content-wrapper">
+                  <div className="text-content">
+                    <h2>Slide 2</h2>
+                    <p>Content for Slide 2</p>
+                  </div>
+                  <div className="image-content">
+                    <img src="image_url_2" alt="Slide 2 Image" />
+                  </div>
+                </div>
               </div>
             </div>
             <div className="slide slide_3">
               <div className="slide-content">
-                <h2>Slide 3</h2>
-                <p>Content for slide 3</p>
+                <div className="content-wrapper">
+                  <div className="text-content">
+                    <h2>Slide 3</h2>
+                    <p>Content for Slide 3</p>
+                  </div>
+                  <div className="image-content">
+                    <img src="image_url_3" alt="Slide 3 Image" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="slide slide_4">
+              <div className="slide-content">
+                <div className="content-wrapper">
+                  <div className="text-content">
+                    <h2>Slide 4</h2>
+                    <p>Content for Slide 4</p>
+                  </div>
+                  <div className="image-content">
+                    <img src="image_url_4" alt="Slide 4 Image" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -35,11 +70,13 @@ const Slider = () => {
         <label htmlFor="slide1"></label>
         <label htmlFor="slide2"></label>
         <label htmlFor="slide3"></label>
+        <label htmlFor="slide4"></label>
       </div>
       <div id="bullets">
         <label htmlFor="slide1"></label>
         <label htmlFor="slide2"></label>
         <label htmlFor="slide3"></label>
+        <label htmlFor="slide4"></label>
       </div>
     </div>
   );
