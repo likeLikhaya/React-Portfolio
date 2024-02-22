809d654e11b985c8037bcdde54d84f1be26e5a80
Likhaya <kalimashelikhaya3@gmail.com>
2024-02-22 21:56:39 +0200

    errors

--- react-portfolio-project/src/App.jsx
+++ react-portfolio-project/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 
 //simport About from "./components/About/About";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
 import Welcome from "./components/Welcome/Welcome";
 import Skills from "./components/Skills/Skills";
 import Contact from "./components/Contact/Contact";
@@ -11,13 +13,14 @@ import Slider from "./components/Slider/Slider";
 function App() {
   return (
     <>
+      <Navbar />
       <Welcome />
-
+      <About />
       <Skills />
       <Slider />
       <Contact />
       <Footer />
-      {/*  <About />*/}
+      {/*  */}
     </>
   );
 }
--- react-portfolio-project/src/components/Welcome/Welcome.jsx
+++ react-portfolio-project/src/components/Welcome/Welcome.jsx
@@ -4,167 +4,25 @@ import "./Welcome.css";
 const Welcome = () => {
   return (
     <>
-      <div
-        className="Welcome"
-        style={{
-          width: 1440,
-          height: 1024,
-          position: "relative",
-          background: "#BAA6F5",
-        }}
-      >
-        <div
-          className="intro"
-          style={{
-            width: 805,
-            height: 454,
-            left: 302,
-            top: 346,
-            position: "absolute",
-            textAlign: "center",
-            color: "black",
-            fontSize: 25,
-            fontFamily: "Monda",
-            fontWeight: "400",
-            wordWrap: "break-word",
-          }}
-        >
-          Welcome to my Digital Wonderland! 🚀 <br />
-          <br />
-          This is where pixels come alive, code speaks, and innovation reigns
-          supreme! Get ready to embark on an adventure where every project is a
-          story waiting to be told- a tale of precision, problem-solving skills,
-          and K-drama charm. Here, The Office isn't just a show; it's a
-          wellspring of inspiration. Are you ready to explore the extraordinary?
-          Let the digital adventure begin!
+      <section>
+        <div className="welcome-container">
+          <div className="welcome-title">
+            <div className="overlap-group">
+              <p className="welcome">
+                Welcome to my Digital Wonderland! 🚀
+                <br />
+                This is where pixels come alive, code speaks, and innovation
+                reigns supreme! Get ready to embark on an adventure where every
+                project is a story waiting to be told- a tale of precision,
+                problem-solving skills, and K-drama charm. Here, The Office
+                isn't just a show; it's a wellspring of inspiration. Are you
+                ready to explore the extraordinary? Let the digital adventure
+                begin!
+              </p>
+            </div>
+          </div>
         </div>
-        <div
-          className="HelloThere"
-          style={{
-            width: 417,
-            height: 162,
-            left: 496,
-            top: 223,
-            position: "absolute",
-            textAlign: "center",
-            color: "black",
-            fontSize: 62,
-            fontFamily: "Monda",
-            fontWeight: "400",
-            wordWrap: "break-word",
-          }}
-        >
-          Hello There !
-        </div>
-        <div
-          className="Kalimashe"
-          style={{
-            width: 230,
-            height: 67,
-            left: 115,
-            top: 65,
-            position: "absolute",
-            textAlign: "center",
-            color: "black",
-            fontSize: 32,
-            fontFamily: "Monda",
-            fontWeight: "400",
-            wordWrap: "break-word",
-          }}
-        >
-          KALIMASHE
-        </div>
-        <div
-          className="Welcome"
-          style={{
-            width: 123,
-            height: 57,
-            left: 414,
-            top: 70,
-            position: "absolute",
-            textAlign: "center",
-            color: "black",
-            fontSize: 17,
-            fontFamily: "Monda",
-            fontWeight: "400",
-            wordWrap: "break-word",
-          }}
-        >
-          Welcome
-        </div>
-        <div
-          className="MeetLikhaya"
-          style={{
-            width: 125,
-            height: 44,
-            left: 570,
-            top: 77,
-            position: "absolute",
-            textAlign: "center",
-            color: "black",
-            fontSize: 17,
-            fontFamily: "Monda",
-            fontWeight: "400",
-            wordWrap: "break-word",
-          }}
-        >
-          Meet Likhaya
-        </div>
-        <div
-          className="TalentBank"
-          style={{
-            width: 125,
-            height: 48,
-            left: 727,
-            top: 76,
-            position: "absolute",
-            textAlign: "center",
-            color: "black",
-            fontSize: 17,
-            fontFamily: "Monda",
-            fontWeight: "400",
-            wordWrap: "break-word",
-          }}
-        >
-          Talent Bank
-        </div>
-        <div
-          className="Creations"
-          style={{
-            width: 125,
-            height: 44,
-            left: 875,
-            top: 77,
-            position: "absolute",
-            textAlign: "center",
-            color: "black",
-            fontSize: 17,
-            fontFamily: "Monda",
-            fontWeight: "400",
-            wordWrap: "break-word",
-          }}
-        >
-          Creations
-        </div>
-        <div
-          className="LetSGetInTouch"
-          style={{
-            width: 176,
-            height: 44,
-            left: 1037,
-            top: 76,
-            position: "absolute",
-            textAlign: "center",
-            color: "black",
-            fontSize: 17,
-            fontFamily: "Monda",
-            fontWeight: "400",
-            wordWrap: "break-word",
-          }}
-        >
-          Let’s get in touch
-        </div>
-      </div>
+      </section>
     </>
   );
 };
