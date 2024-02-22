1b4114b218574baa540b4ba35d677b02f47d2867
Likhaya <kalimashelikhaya3@gmail.com>
2024-02-22 10:49:29 +0200

    trying to implement download cv button

--- react-portfolio-project/src/App.jsx
+++ react-portfolio-project/src/App.jsx
@@ -1,24 +1,22 @@
 import "./App.css";
 //import Skills from "./components/Skills/Skills";
 import About from "./components/About/About";
-// import Welcome from "./components/Welcome/Welcome";
+//import Welcome from "./components/Welcome/Welcome";
 //import Slider from "./components/Slider/Slider";
 //import Footer from "./components/Footer/Footer";
-// import Contact from "./components/Contact/Contact";
-
-import "./index.css";
+//import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <>
       <About />
+
       {/* 
       <Welcome />
-      
-     <Skills />
-          <Slider />
-   <Contact />
-    <Footer />
+      <Skills />
+      <Slider />
+      <Contact />
+      <Footer />
       
       
         */}
--- react-portfolio-project/src/components/About/About.jsx
+++ react-portfolio-project/src/components/About/About.jsx
@@ -3,37 +3,33 @@ import "./About.css";
 
 const About = () => {
   return (
-    <>
-      <div className="heading">
-        <h1>Meet Likhaya</h1>
+    <div className="desktop">
+      <div className="div">
+        <img
+          className="profilepic"
+          alt="Profilepic"
+          src="src/images/profile-pic.png"
+        />
+        <div className="text-wrapper">Meet Likhaya!</div>
+        <p className="i-am-a-skilled-coder">
+          I am a skilled coder who creates innovative products using
+          caffeine-powered code spells. During the day, I work in the tech
+          industry, transforming computer code into beautiful, functional
+          systems. When I'm not working, I love to watch K-dramas and The Office
+          for a good laugh.
+          <br />
+          <br />
+          But sometimes I need a break from the digital world, and that's when I
+          head to the beach. The soothing sound of the waves and the fresh sea
+          air helps me de-stress and recharge. It's a peaceful escape from the
+          complexity of coding.
+          <br />
+          <br />
+          Throughout all of this, my friends and family kept me grounded. They
+          support me and keep me sane in the ever-changing world of tech.
+        </p>
       </div>
-      <div className="container">
-        <section className="about">
-          <div className="about-image">
-            <img src="src/images/profile-pic.png" alt="" />
-          </div>
-          <div className="about-content">
-            <p>
-              I am a skilled coder who creates innovative products using
-              caffeine-powered code spells. During the day, I work in the tech
-              industry, transforming computer code into beautiful, functional
-              systems. When I'm not working, I love to watch K-dramas and The
-              Office for a good laugh. But sometimes I need a break from the
-              digital world, and that's when I head to the beach. The soothing
-              sound of the waves and the fresh sea air helps me de-stress and
-              recharge. It's a peaceful escape from the complexity of coding.
-              Throughout all of this, my friends and family keep me grounded.
-              They support me and keep me sane in the ever-changing world of
-              tech.
-            </p>
-            <a href="" className="download-cv">
-              {" "}
-              Download CV
-            </a>
-          </div>
-        </section>
-      </div>
-    </>
+    </div>
   );
 };
 
