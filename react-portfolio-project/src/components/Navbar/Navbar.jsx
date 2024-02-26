370805edf18757e573d4752e67e49cd8e839d8e3
Likhaya <99352117+likeLikhaya@users.noreply.github.com>
2024-02-26 15:20:20 +0200

    hosting cache

--- react-portfolio-project/src/components/Navbar/Navbar.jsx
+++ react-portfolio-project/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
